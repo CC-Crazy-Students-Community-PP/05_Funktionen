ad3567bc30162609b6a8653273630440ae914967
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-11 10:08:34 +0100

    Funktionen 01

--- script_01.js
+++ script_01.js
@@ -1,4 +1,21 @@
 "use strict";
+/***********************************************************************/
+/*********                    Funktionen 01                   **********/
+/***********************************************************************/
 
-/***** Funktionen 01 *****/
+    // Funktionsdeklaration (body | callee)
+    function greatings() {
+        console.log( "Hallo Programmierer!" );      // Der Block der Funktion, das was passieren soll, wenn Aufruf stattfindet
+    }
 
+    // Funktionsaufruf (call), aufrufbar von jeder Position in der Datei, auch vor der Funktionsdeklaration
+    greatings();                                    // Die Funktion passiert erst , wenn der Aufruf startet, wenn kein Aufruf, dann keine Funktion
+
+
+/***********************************************************************/
+/*********                    Funktionen 02                   **********/
+/***********************************************************************/
+
+    function name() {
+        
+    }
