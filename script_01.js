8114f4d166dffb0dc3b3d2757cf0967a2ce9aa57
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-11 10:46:25 +0100

    02-a Parametisierung und Datenübergabe von außen

--- script_01.js
+++ script_01.js
@@ -5,17 +5,30 @@
 
     // Funktionsdeklaration (body | callee)
     function greatings() {
-        console.log( "Hallo Programmierer!" );      // Der Block der Funktion, das was passieren soll, wenn Aufruf stattfindet
+        console.log( "Hallo Programmierer!" );                  // Der Block der Funktion, das was passieren soll, wenn Aufruf stattfindet
     }
 
     // Funktionsaufruf (call), aufrufbar von jeder Position in der Datei, auch vor der Funktionsdeklaration
-    greatings();                                    // Die Funktion passiert erst , wenn der Aufruf startet, wenn kein Aufruf, dann keine Funktion
+    greatings();                                                // Die Funktion passiert erst , wenn der Aufruf startet, wenn kein Aufruf, dann keine Funktion
 
 
 /***********************************************************************/
 /*********                    Funktionen 02                   **********/
 /***********************************************************************/
 
-    function name() {
-        
+    // 02-a Parametisierung und Datenübergabe von innen
+    ausgabeName1();
+
+    function ausgabeName1() {
+        let firstName1 = "Hosrt"                                    // bleibt nur innerhalb der Funktion
+        console.log( "Hallo " + firstName1 + "!" );
     }
+
+    // console.log( firstName1 );                                     // Fehler: Scpoe (Geltungsbereich)
+
+    // 02-a Parametisierung und Datenübergabe von außen
+    function ausgabeName2(firstName1) {                             // Parameter, die gleich beim Nutzen deklariert werden
+        console.log( "Hallo " + firstName1 + "!" );
+    }
+
+    ausgabeName2("Sabine");                                         // Die Ausgabe startet mit dem Aufruf und gibt die Variable als Argument mit
